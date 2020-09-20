d4925de63375263f0549b5fe42b2c38d242eb84c
Latisha Joe <latisha_joe@yahoo.com>
2020-09-20 05:13:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
-class Dog 
+class Dog
 
    def name=(dogs_name)
-     @name= dogs_name 
-   end 
+     @name= dogs_name
+   end
 
-   def name 
-     @name 
-   end 
+   def name
+     @name
+   end
 
    def breed=(breed)
-     @breed = breed 
-   end 
+     @breed = breed
+   end
 
-   def breed 
-     @breed 
-   end 
+   def breed
+     @breed
+   end
 
  end 
